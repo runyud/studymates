9e4cbc170ce06d1ebd7832091fd186fdbaf4ac7d
saican24 <33812957+saican24@users.noreply.github.com>
2020-03-28 04:09:05 -0400

    M-F availability updates

--- add_requests.php
+++ add_requests.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = new PDO('mysql:host=localhost;dbname=studymates', 'root', '');
+$connect = new PDO('mysql:host=localhost ;dbname=studymates', 'root', '');
 
 $error = '';
 $user_name = '';
@@ -8,6 +8,11 @@ $user_email = '';
 $location = '';
 $open_from = '';
 $open_until = '';
+$Monday = '';
+$Tuesday = '';
+$Wednesday = '';
+$Thursday = '';
+$Friday = '';
 //$comment_name = '';
 //$comment_content = '';
 
@@ -56,12 +61,57 @@ else
 	$open_until = $_POST["open_until"];
 }
 
+if(empty($_POST["Monday"]))
+{
+  $error .= '<p class="text-danger">Availability is required</p>';
+}
+else
+{
+  $Monday = $_POST["Monday"];
+}
+
+if(empty($_POST["Tuesday"]))
+{
+  $error .= '<p class="text-danger">Availability is required</p>';
+}
+else
+{
+  $Tuesday = $_POST["Tuesday"];
+}
+
+if(empty($_POST["Wednesday"]))
+{
+  $error .= '<p class="text-danger">Availability is required</p>';
+}
+else
+{
+  $Wednesday = $_POST["Wednesday"];
+}
+
+if(empty($_POST["Thursday"]))
+{
+  $error .= '<p class="text-danger">Availability is required</p>';
+}
+else
+{
+  $Thursday = $_POST["Thursday"];
+}
+
+if(empty($_POST["Friday"]))
+{
+  $error .= '<p class="text-danger">Availability is required</p>';
+}
+else
+{
+  $Friday = $_POST["Friday"];
+}
+
 if($error == '')
 {
  $query = "
  INSERT INTO requests 
- (name, email, location, open_from, open_until) 
- VALUES (:name, :email, :location, :open_from, :open_until)
+ (name, email, location, open_from, open_until, Monday, Tuesday, Wednesday, Thursday, Friday) 
+ VALUES (:name, :email, :location, :open_from, :open_until, :Monday, :Tuesday, :Wednesday, :Thursday, :Friday)
  ";
  $statement = $connect->prepare($query);
  $statement->execute(
@@ -70,7 +120,12 @@ if($error == '')
    ':email'    => $user_email,
    ':location' => $location,
    ':open_from' => $open_from,
-   ':open_until' => $open_until
+   ':open_until' => $open_until,
+   ':Monday' => $Monday,
+   ':Tuesday' => $Tuesday,
+   ':Wednesday' => $Wednesday,
+   ':Thursday' => $Thursday,
+   ':Friday' => $Friday
   )
  );
  $error = '<label class="text-success"></label>';
